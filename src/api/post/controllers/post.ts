ae523df3479b763d34718385573127c8aac170e6
Rodrigo <rdg.hora1@gmail.com>
2023-12-04 13:06:53 -0300

    Relacao usuario post

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -4,4 +4,16 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController("api::post.post");
+export default factories.createCoreService("api::post.post", {
+  async create(ctx: any) {
+    const response = await super.create({
+      data: {
+        publishedAt: new Date(),
+        user: ctx.state.user.id,
+        title: ctx.request.body.data.title,
+        content: ctx.request.body.data.content
+      }
+    });
+    return response
+  },
+});
