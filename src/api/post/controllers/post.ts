990bac602d2ebccb505b6b7f3d10c4acf65e283d
Rodrigo <rdg.hora1@gmail.com>
2023-12-04 13:16:58 -0300

    Rota de count criada

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -5,15 +5,26 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreService("api::post.post", {
+  // Autenticação do usuário com o post
   async create(ctx: any) {
+    const { id } = ctx.state.user;
+    const { title, content } = ctx.request.body.data;
+    const publishedAt = new Date();
+    const post = { title, content, user: id, publishedAt };
+
     const response = await super.create({
       data: {
-        publishedAt: new Date(),
-        user: ctx.state.user.id,
-        title: ctx.request.body.data.title,
-        content: ctx.request.body.data.content
-      }
+        post,
+      },
     });
-    return response
+    return response;
+  },
+
+  // Contador de Posts
+  count(ctx) {
+    const { query } = ctx.request;
+    return strapi.query("api::post.post").count({ where: query });
   },
 });
+
+
---
+++ src/api/post/routes/count-post.ts
@@ -0,0 +1,9 @@
+export default {
+  routes: [
+    {
+      method: "GET",
+      path: "/posts/count",
+      handler: "post.count",
+    },
+  ],
+};
